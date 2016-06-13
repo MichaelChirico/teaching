c52dbe8292ec8106b20db232b54129aa2ab8bf03
Michael Chirico <MichaelChirico4@gmail.com>
2016-06-13 23:38:31 +0800

    adding hw 2 solutions

--- xian_2016/302_macro/course_page_make_copy.sh
+++ xian_2016/302_macro/course_page_make_copy.sh
@@ -4,3 +4,5 @@ cd /home/michael/Documents/teaching/xian_2016/302_macro/
 pandoc -s -o course_page.html course_page.md
 
 cp course_page.html /home/michael/githubio/xian/macro/
+
+cp "$@" /home/michael/githubio/xian/macro
