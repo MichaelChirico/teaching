369d93ebd8122667bc0f9146948d20c95c3b3ffe
Michael Chirico <MichaelChirico4@gmail.com>
2016-06-13 22:59:49 +0800

    added hw 2 solution; improved capabilities of course page script

--- xian_2016/301_micro/course_page_make_copy.sh
+++ xian_2016/301_micro/course_page_make_copy.sh
@@ -4,3 +4,5 @@ cd /home/michael/Documents/teaching/xian_2016/301_micro/
 pandoc -s -o course_page.html course_page.md
 
 cp course_page.html /home/michael/githubio/xian/micro/
+
+cp "$@" /home/michael/githubio/xian/micro
